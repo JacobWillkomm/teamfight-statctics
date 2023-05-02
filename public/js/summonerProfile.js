a63b4a79d4834c48bb2d2ce3b5c4f9d6b3ebbc96
Jacob Willkomm <jacobwillkomm@gmail.com>
2023-05-02 15:39:23 -0700

    Fix pagination

--- public/js/summonerProfile.js
+++ public/js/summonerProfile.js
@@ -14,3 +14,33 @@ for(let i = 0; i < setFilters.length; i++){
 
     })
 }
+
+let pages = document.querySelector(".pagination").children
+
+for(let i = 0; i < pages.length; i++){
+    let page = pages[i];
+    page.addEventListener("click", function(){
+        //For each page, remove "page-selected" from classlist
+        for(let i = 0; i < pages.length; i++){
+            pages[i].classList.remove("page-selected")
+        }
+        //Add "page-selected" to this one
+        page.classList.add("page-selected")
+
+        //Update matches
+        updateMatchDisplay(page.innerHTML)
+
+    })
+}
+
+function updateMatchDisplay(pageIndex){
+    for(let matchIndex = 0; matchIndex < matches.length; matchIndex++){
+        if(matchIndex >= ((+pageIndex - 1) * 10) && matchIndex < ((+pageIndex) * 10)){
+            matches[matchIndex].style.display = "flex"
+        }else{
+            matches[matchIndex].style.display = "none"
+        }
+    }
+}
+let matches = document.querySelector(".match-list").children
+console.log(matches)
